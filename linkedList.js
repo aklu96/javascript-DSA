1169eb2be7488b77f1d30d410e5e30b5788c478a
Alex Klyuev <aklyuev96@gmail.com>
2021-07-07 17:05:56 -1000

    linked list

--- linkedList.js
+++ linkedList.js
@@ -1,1 +1,28 @@
-// to-do
+class ListNode {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+class List {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+  }
+
+  addToTail(val) {
+    const node = new ListNode(val);
+    if (this.head === null) {
+      this.head = node;
+      this.tail = node;
+    } else {
+      this.tail.next = node;
+      this.tail = node;
+    }
+  }
+
+  removeHead() {
+    this.head = this.head.next;
+  }
+}
