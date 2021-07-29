d2bbc990d054b0d1a3654079bffb23cfc2c2e5c2
Alex Klyuev <aklyuev96@gmail.com>
2021-07-28 18:01:23 -0700

    update queue class to dequeue in O(n) time

--- queue.js
+++ queue.js
@@ -1,34 +1,40 @@
 class Queue {
-
+  // a line has a front and an end
+  // we remove from the front and add to the end
+  // store all queue elements in storage and maintain front and end pointers
   constructor() {
+    this.front = 0;
+    this.end = 0;
     this.storage = {};
-    this.index = 0;
   }
 
   enqueue(value) {
-    this.storage[this.index] = value;
-    this.index++;
+    if (!this.end) {
+      this.storage[1] = value;
+      this.end = 1;
+      this.front = 1;
+      return;
+    }
+
+    this.end++;
+    this.storage[this.end] = value;
   }
 
   dequeue() {
-    this.index--;
-    var deletedItem = this.storage[0];
-    delete this.storage[0];
-    for (var key in this.storage) {
-      this.storage[key - 1] = this.storage[key];
+    const value = this.storage[this.front];
+    delete this.storage[this.front];
+    if (this.front === this.end) {
+      this.front = null;
+      this.end = null;
+      return value;
     }
-    delete this.storage[this.index];
-    return deletedItem;
+    this.front++;
+    return value;
   }
 
   size() {
-    var size = 0;
-    for (var key in this.storage) {
-      if (this.storage[key]) {
-        size++;
-      }
-    }
-    return size;
+    if (!this.end) return 0;
+    return this.end - this.front + 1;
   }
 
 }
